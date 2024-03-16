2cd856723b5e7eff7b4ebbc9a4a9ec9ac4b57a09
Carlos Eduardo Dev <kadu0410@gmail.com>
2024-03-16 11:45:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-const estadoCivilRadios = document.querySelectorAll('input[name="estado_civil"]');
-estadoCivilRadios.forEach(radio => {
-    radio.addEventListener('click', function () {
-        estadoCivilRadios.forEach(otherRadio => {
-            if (otherRadio !== radio) {
-                otherRadio.checked = false;
-            }
-        });
-    });
-});
-
-const recebidoPorRadios = document.querySelectorAll('input[name="recebido_por"]');
-estadoCivilRadios.forEach(radio => {
-    radio.addEventListener('click', function () {
-        estadoCivilRadios.forEach(otherRadio => {
-            if (otherRadio !== radio) {
-                otherRadio.checked = false;
-            }
-        });
-    });
-});
+$(function(){
+    $('input.checkgroup').click(function(){
+       if($(this).is(":checked")){
+          $('input.checkgroup').attr('disabled',true);
+          $(this).removeAttr('disabled');
+       }else{
+          $('input.checkgroup').removeAttr('disabled');
+       }
+    })
+ })
